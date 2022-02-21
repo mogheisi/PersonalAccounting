a0c7a9863424ea3a513323a034ff8cac3401b86d
Shantia <shantia.mogheisi@outlook.com>
2022-02-21 03:35:25 +0330

    add git ignore

--- main/models.py
+++ main/models.py
@@ -1,3 +1,19 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+
+class Expense(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    amount = models.BigIntegerField()
+    date = models.DateTimeField()
+    description = models.CharField(max_length=256)
+    def __unicode__(self):
+        return '{}-{}-{}'.format(self.user, self.amount, self.date)
+
+
+class Income(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    amount = models.BigIntegerField()
+    date = models.DateTimeField()
+    description = models.CharField(max_length=256)
+
